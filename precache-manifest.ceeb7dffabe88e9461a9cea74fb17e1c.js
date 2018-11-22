420d53456fc5c3ec733b366c98258d2a9bbd3ab5
Bruno <zettca@gmail.com>
2018-11-22 18:29:31 +0000

    Updates

---
+++ precache-manifest.ceeb7dffabe88e9461a9cea74fb17e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a452d3f0f5368698bcf1",
+    "url": "./static/js/main.a452d3f0.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "./static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "a452d3f0f5368698bcf1",
+    "url": "./static/css/main.96c897ed.chunk.css"
+  },
+  {
+    "revision": "8f073eac99a5c5ab207f8137b28c96e5",
+    "url": "./index.html"
+  }
+];
